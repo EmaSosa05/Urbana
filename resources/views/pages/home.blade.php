4a6b47a8fd1e4abefc4fa84aaf446b1305b64d35
Emanuel Sosa <ema_sosa_2@hotmail.com>
2020-09-24 19:32:56 -0300

    Urbana 0.2

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,24 +2,25 @@
  
 @section('content')
     <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
-    <div class="carousel-inner">
-        <div class="carousel-item active">
-        <img class="d-block w-100" src="https://images.clarin.com/2018/01/18/B15vQiRNG_720x0__1.jpg" alt="First slide">
+        <div class="carousel-inner">
+            <img src="images/Urbana_logo.png">
+            <div class="carousel-item active">
+                <img class="d-block w-100" src="https://images.clarin.com/2018/01/18/B15vQiRNG_720x0__1.jpg" alt="First slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="https://www.lanoticia1.com/sites/default/files/circ.jpeg" alt="Second slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="https://www.infocanuelas.com/app/media/acercarte-canuelas.jpeg" alt="Third slide">
+            </div>
         </div>
-        <div class="carousel-item">
-        <img class="d-block w-100" src="https://www.lanoticia1.com/sites/default/files/circ.jpeg" alt="Second slide">
-        </div>
-        <div class="carousel-item">
-        <img class="d-block w-100" src="https://www.infocanuelas.com/app/media/acercarte-canuelas.jpeg" alt="Third slide">
-        </div>
-    </div>
-    <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-    </a>
-    <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-    </a>
+        <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
     </div>
 @endsection
