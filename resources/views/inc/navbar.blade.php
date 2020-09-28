0211401cec10b6f344268b6113a64d02ab3483f3
Emanuel Sosa <ema_sosa_2@hotmail.com>
2020-09-28 19:01:56 -0300

    Urbana 0.6

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -4,6 +4,9 @@
       <img src="/images/Urbana_logo_final.ico" width="30" height="30" class="d-inline-block align-top" alt="">
       Urbana SRL
     </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav ml-auto">
         <li class="nav-item active">
--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -2,68 +2,59 @@
 
 @section('about')
 <section id="nosotros" class="about-parallax">
-    <div class="content">
-        <h1>Somos una empresa que birnda servicio y soluciones en el desarrollo de la construcción y el paisajismo.</h1>
-    </div>
 </section>
 <section class="about-hero">
     <div class="content">
-        <div class="row">
-            <div class="col">
-                <div class="paral-de">
-                    <img src="https://images.clarin.com/2018/01/18/B15vQiRNG_720x0__1.jpg" alt="">
+        <div class="row no-gutters">
+            <div class="col-sm">
+                <img class="w-100" src="https://images.clarin.com/2018/01/18/B15vQiRNG_720x0__1.jpg" alt="">
+            </div>
+            <div class="col-sm my-auto">
+                <div class="col-content text-left">
+                    <h1>Nosotros</h1>
+                    <hr class="separador float-left">
+                    <br><p>Somos una empresa que brinda servicio y soluciones en el desarrollo de la construcción y el paisajismo.
+                    Nos destacamos en proponer soluciones innovadoras y creativas aplicadas a una gran variedad de escalas y entornos, participando en el proceso, ejecucion y mantenimeinto.</p>
+                    <p>Mantenemos un equipo de trabajo conformado por personal capacitado para el buen desarrollo de las tareas, asegurando asi la seguridad, la eficacio y el cuidado del ambiente.
+                    Nuestro esfuerzo se refleja en obras, tanto privadas como públicas, que mejoran la calidad de vida de las personas y agregan valor a nuetros clientes.</p>
                 </div>
-            </div>
-            <div class="col col-center">
-                <p>Nos destacamos en proponer soluciones innovadoras y creativas aplicadas a una gran variedad de escalas y entornos, participando en el proceso, ejecucion y mantenimeinto.</p>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col col-center">
-                <p>Mantenemos un equipo de trabajo conformado por personal capacitado para el buen desarrollo de las tareas, asegurando asi la seguridad, la eficacio y el cuidado del ambiente.</p>
-            </div>
-            <div class="col">
-                <div class="paral-iz">
-                    <img src="https://www.lanoticia1.com/sites/default/files/circ.jpeg" alt="">
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col">
-                <div class="paral-de">
-                    <img src="https://www.infocanuelas.com/app/media/acercarte-canuelas.jpeg" alt="">
-                </div>
-            </div>
-            <div class="col col-center">
-                <p>Nuestro esfuerzo se refleja en obras, tanto privadas como públicas, que mejoran la calidad de vida de las personas y agregan valor a nuetros clientes.</p>
             </div>
         </div>
     </div>
 </section>
-<section class="about-hero">
+
+<section class="about-hero" style="background: url('https://disruptivo.tv/wp-content/uploads/2019/04/team.png');">
     <div class="content-2">
-        <div class="row">
-            <div class="col">
+        <div class="row no-gutters">
+            <div class="col-sm">
                 <div class="d-flex justify-content-center">
-                    <div class="photo-background">
-                        <img src="https://image.freepik.com/vector-gratis/perfil-avatar-hombre-icono-redondo_24640-14044.jpg">
-                        <h1>GUILLERMO</h1>
-                        <h1>RAMOS</h1>
-                        <p>SOCIO-GERENTE EMRPESA URBANA S.R.L.</p>
-                        <h5>1540251592</h5>
-                        <h5>atw061@gmail.com</h5>
+                    <div class="row photo-background">
+                        <div class="col">
+                            <img src="https://image.freepik.com/vector-gratis/perfil-avatar-hombre-icono-redondo_24640-14044.jpg">    
+                        </div>
+                        <div class="col">
+                            <h1>GUILLERMO</h1>
+                            <h1>RAMOS</h1>
+                            <p>SOCIO-GERENTE EMRPESA URBANA S.R.L.</p>
+                            <h5>1540251592</h5>
+                            <h5>atw061@gmail.com</h5>
+                        </div>
                     </div>
                 </div>                
             </div>
-            <div class="col">
+            <div class="col-sm">
                 <div class="d-flex justify-content-center">
-                    <div class="photo-background">
-                        <img src="https://image.freepik.com/vector-gratis/perfil-avatar-hombre-icono-redondo_24640-14044.jpg">
-                        <h1>RUBEN</h1>
-                        <h1>SOLANO</h1>
-                        <p>SOCIO-GERENTE EMRPESA URBANA S.R.L.</p>
-                        <h5>1568355864</h5>
-                        <h5>rubenpsolano@gmail.com</h5>
+                    <div class="row photo-background">
+                        <div class="col">
+                            <img src="https://image.freepik.com/vector-gratis/perfil-avatar-hombre-icono-redondo_24640-14044.jpg">
+                        </div>
+                        <div class="col">
+                            <h1>RUBEN</h1>
+                            <h1>SOLANO</h1>
+                            <p>SOCIO-GERENTE EMRPESA URBANA S.R.L.</p>
+                            <h5>1568355864</h5>
+                            <h5>rubenpsolano@gmail.com</h5>
+                        </div>
                     </div>
                 </div>
             </div>
